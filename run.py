31a6c7263bb8af4cc1bd3f5905d62ef765ef2950
MUZI <98746157@qq.com>
2020-07-05 17:13:45 +0800

    增加

--- run.py
+++ run.py
@@ -2,7 +2,11 @@
 # @Time    : 2020/7/3 19:18
 # @Author  : muzijie
 # @File    : run.py
-
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from autowork.http_request import http_request
 from autowork.rw_excel import read_data
 from autowork.rw_excel import  write_data
